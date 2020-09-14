a08787afcba008dbaa6bb7d461a880dc39d8f9f7
Vitor Lupinetti <vitorlupinetti@MacBook-Air-de-Vitor.local>
2020-09-14 00:01:19 -0300

    pagina de listagem de repositorios

--- platforms/ios/www/js/projeto1.js
+++ platforms/ios/www/js/projeto1.js
@@ -34,7 +34,7 @@ function Append(data) {
                                     <p class="card-text">Seguindo: ${data.following}</p>
                                     <p class="card-text">Repositórios: ${data.public_repos}</p>
                                     ${bio}
-                                    <a href="#" class="btn btn-primary">Repositórios</a>
+                                    <a href="projeto1.repositories.html?${data.login}" class="btn btn-primary">Repositórios</a>
                                     </div>
                                 </div>`;
 
@@ -44,3 +44,82 @@ function Append(data) {
     let container = document.querySelector('#card-div');
     container.innerHTML = html;
 }    
+
+
+
+function CapturaParametrosUrl() {
+
+    //captura a url da página
+    var url = window.location.href; 
+	
+	//tenta localizar o ?
+    var res = url.split('?'); 
+    	
+	if (res[1] === undefined) {
+        alert('página sem parâmetros.');
+    }
+	
+    if (res[1] !== undefined) {
+        //tenta localizar os & (pode haver mais de 1)
+        return res[1];
+		
+    }
+}
+
+
+
+function callApiRepo() {
+
+
+    var url = 'https://api.github.com/users/' + CapturaParametrosUrl() + '/repos';
+
+
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
+        .then(resposta => resposta.json())
+        .then(resposta => AppendRepo(resposta))
+        .catch(erro => console.log(erro));
+
+}
+
+
+
+function AppendRepo(data) {
+    let html = '';
+    data.map(repo => {
+        let htmlSegment = ` <div class="card ">
+                                <div class="card-body card-flex">
+                                    <img class="card-img-top" src="./img/github.png">
+                                    <p class="card-title">Nome: ${repo.name}</p>
+                                    <p>Autor: ${repo.owner.login}</p>
+                                    <p>Issues: ${repo.open_issues}   
+                                    <p>Stars: ${repo.stargazers_count}</p>
+                                    <p>Forks ${repo.forks_count}</p>
+                                    <a href= ${repo.html_url} class="btn btn-info">Abrir no git</a>
+                                </div>
+                            </div> `;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.main-container-repos');
+    container.innerHTML = html;
+}
+
+function CapturaParametrosUrl() {
+
+    //captura a url da página
+    var url = window.location.href;
+
+    //tenta localizar o ?
+    var res = url.split('?');
+
+    if (res[1] === undefined) {
+        alert('página sem parâmetros.');
+    }
+
+    if (res[1] !== undefined) {
+        //tenta localizar os & (pode haver mais de 1)
+        return res[1];
+
+    }
+}
