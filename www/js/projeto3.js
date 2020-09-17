b5ec03da461d632a3bc9d9eeb7cd8668bd0c16a2
Vitor Lupinetti <vitorlupinetti@MacBook-Air-de-Vitor.local>
2020-09-17 20:48:38 -0300

    tela de curso da feature 3

--- www/js/projeto3.js
+++ www/js/projeto3.js
@@ -1,16 +1,16 @@
-function listaFormacoes(){
+function listaFormacoes() {
     var categoria = CapturaParametrosUrl();
     let html = '';
-    if(categoria === 'mobile'){
+    if (categoria === 'mobile') {
         let htmlSegment = ` <div class="card ">
                                 <div class="card-body card-flex">
                                     <h3 class="card-title text-center">Formações mobile</h3>
                                     <img class="card-img-top" src="./img/mobile.png">
-                                    <a class="formacao">Android</a>
+                                    <a href='projeto3.cursos.html?android' class="formacao">Android</a>
                                     <hr>
-                                    <a class="formacao">IOS</a>   
+                                    <a href='projeto3.cursos.html?ios' class="formacao">IOS</a>   
                                     <hr>
-                                    <a class="formacao">Multiplataforma</a>
+                                    <a href='projeto3.cursos.html?xamarin' class="formacao">Multiplataforma</a>
                                 </div>
                             </div> `;
 
@@ -18,16 +18,16 @@ function listaFormacoes(){
         let container = document.querySelector('.content');
         container.innerHTML = html;
     }
-    else if(categoria === 'programacao'){
+    else if (categoria === 'programacao') {
         let htmlSegment = ` <div class="card ">
                                 <div class="card-body card-flex">
                                     <h3 class="card-title text-center">Formações de programação</h3>
                                     <img class="card-img-top" src="./img/programacao.png">
-                                    <a class="formacao">Java</a>
+                                    <a href='projeto3.cursos.html?java' class="formacao">Java</a>
                                     <hr>
-                                    <a class="formacao">.NET</a>   
+                                    <a href='projeto3.cursos.html?dotnet' class="formacao">.NET</a>   
                                     <hr>
-                                    <a class="formacao">Node Js</a>
+                                    <a href='projeto3.cursos.html?node-js-12' class="formacao">Node Js</a>
                                 </div>
                             </div> `;
 
@@ -35,16 +35,16 @@ function listaFormacoes(){
         let container = document.querySelector('.content');
         container.innerHTML = html;
     }
-    else if(categoria === 'frontend'){
+    else if (categoria === 'frontend') {
         let htmlSegment = ` <div class="card ">
                                 <div class="card-body card-flex">
                                     <h3 class="card-title text-center">Formações de front-end</h3>
                                     <img class="card-img-top" src="./img/programacao.png">
-                                    <a class="formacao">Angular</a>
+                                    <a href='projeto3.cursos.html?angular' class="formacao">Angular</a>
                                     <hr>
-                                    <a class="formacao">React Js</a>   
+                                    <a href='projeto3.cursos.html?react-js' class="formacao">React Js</a>   
                                     <hr>
-                                    <a class="formacao">HTML e CSS</a>
+                                    <a href='projeto3.cursos.html?html-e-css' class="formacao">HTML e CSS</a>
                                 </div>
                             </div> `;
 
@@ -52,16 +52,16 @@ function listaFormacoes(){
         let container = document.querySelector('.content');
         container.innerHTML = html;
     }
-    else if(categoria === 'datascience'){
+    else if (categoria === 'datascience') {
         let htmlSegment = ` <div class="card ">
                                 <div class="card-body card-flex">
                                     <h3 class="card-title text-center">Formações Data Science</h3>
                                     <img class="card-img-top" src="./img/datascience.png">
-                                    <a class="formacao">Machine Learning</a>
+                                    <a href='projeto3.cursos.html?machine-learning' class="formacao">Machine Learning</a>
                                     <hr>
-                                    <a class="formacao">Python para data science</a>   
+                                    <a href='projeto3.cursos.html?python-data-science' class="formacao">Python para data science</a>   
                                     <hr>
-                                    <a class="formacao">SQL</a>
+                                    <a href='projeto3.cursos.html?microsoft-sql-server-2017' class="formacao">SQL</a>
                                 </div>
                             </div> `;
 
@@ -89,4 +89,72 @@ function CapturaParametrosUrl() {
         return res[1];
 
     }
+}
+
+function listaCursos(formacao) {
+    let html = '';
+    formacao.steps[0].courses.map(curso => {
+        let htmlSegment = ` <div class="card ">
+                                <div class="card-body card-flex">
+                                    <img class="card-img-top" src="./img/programacao.png">
+                                    <p class="card-title">Formação: ${curso.title}</p>
+                                    <a onClick="callApiCurso('${curso.slug}')" class="btn btn-info">Visualizar</a>
+                                </div>
+                            </div> `;
+
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('.cursos');
+    container.innerHTML = html;
+}
+
+
+
+function callApiFormacao() {
+
+
+    var url = 'https://www.alura.com.br/api/formacao-' + CapturaParametrosUrl();
+
+
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
+        .then(resposta => resposta.json())
+        .then(resposta => listaCursos(resposta))
+        .catch(erro => console.log(erro));
+
+}
+
+function callApiCurso(curso) {
+
+
+    var url = 'https://www.alura.com.br/api/curso-' + curso;
+
+
+    fetch(url) // Call the fetch function passing the url of the API as a parameter
+        .then(resposta => resposta.json())
+        .then(resposta => exibeCurso(resposta))
+        .catch(erro => console.log(erro));
+
+}
+
+
+function exibeCurso(data) {
+    let htmlSegment = ` <div class="card ">
+                                <div class="card-body card-flex">
+                                    <h4>Apresentação do curso</h4>
+                                    <video width="300" height="200" class="elasticMedia" controls="" controlslist="nodownload">
+                                        <source src="${data.video_1a_aula}" type="video/mp4">
+                                    </video>
+                                    <p class="card-title"><strong>Curso:</strong> ${data.nome}</p>
+                                    <p><strong>Descrição:</strong> ${data.metadescription}</p>
+                                    <p><strong>Nota do curso:</strong> ${data.nota}</p>
+                                    <p><strong>Carga horária:</strong> ${data.carga_horaria} horas</p>
+                                    <a href="https://www.alura.com.br/curso-online-${data.slug}"class="btn btn-info">Acessar curso no alura</a>
+                                </div>
+                            </div> `;
+
+
+    let container = document.querySelector('.cursos');
+    container.innerHTML = "";
+    container.innerHTML = htmlSegment;
 }
